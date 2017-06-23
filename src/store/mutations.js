1ad20331f7f95f0fcc083104bfe82e69cb48c06b
chanpei <syt37du@gmail.com>
2017-06-24 00:53:33 +0800

    implementation mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,7 +4,23 @@ const REMBER_TIME = "REMBER_TIME"
 const INITIALIZE_DATA = 'INITIALIZE_DATA'
 
 export default {
-    [ADD_ITEMNUM](state, num){
+    [ADD_ITEMNUM](state, num) {
         state.itemNum += num;
+    },
+
+    [REMBER_ANSWER](state, id) {
+        state.answerid.push(id);
+    },
+
+    [REMBER_TIME](state) {
+        state.time = setInterval(() => {
+            state.allTime++;
+        }, 1000)
+    },
+
+    [INITIALIZE_DATA](state) {
+        state.itemNum = 1;
+        state.allTime = 0;
+        state.answerid = [];
     }
 }
